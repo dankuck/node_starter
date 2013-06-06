488c4e0c66d69877f476b682b27b4e9554580934
while1dan <dankuck@gmail.com>
2013-06-06 07:53:49 -0700

    Should be everything needed to make the eval thingie work.

--- app.js
+++ app.js
@@ -12,6 +12,40 @@ var config_file = (process.argv[2] || 'dev') + '_config.js'
   , _ = require('underscore') //underscore.js utl lib
   , util = require('util') //some utilities
   , user_port = process.argv[3] || config.user_port || 8080
+  , admin_pass = config.admin_pass
   ;
 
 app.listen(user_port);
+
+io.sockets.on('connection', function (socket) {
+	
+	  var client_log = function(e){
+	  	console.log(["Sending to the user:", e]);
+	  	try{
+	  		if (e instanceof Error)
+	  			e = e.stack + "";
+	  		else
+	  			e = e + "";
+	  		console.log(e);
+		  	if (socket.is_developer)
+		  	  socket.emit('log', e);
+	    }
+	    catch(ee){
+	    	console.err(['Failed to emit:', ee]);
+	    }
+	  };
+	  
+      socket.on('register_developer',function(password){
+      	if (password == admin_pass)
+      		socket.is_developer = true;
+      	else
+      		socket.disconnect();
+      });
+      
+      socket.on('eval', function(code){
+      	console.log(['eval', code]);
+      	if (! socket.is_developer)
+      		return socket.disconnect();
+      	eval(code);
+      });
+});
